a7de0ee06c168cf9148cf992c1137069333f73b1
Wojtek Siudzinski <admin@suda.pl>
2016-08-31 16:08:28 +0100

    Replace spaces with tabs

--- test/DrayManager.spec.js
+++ test/DrayManager.spec.js
@@ -9,31 +9,31 @@ import bluebird from 'bluebird';
 import {DrayManager, DrayJob} from '../src/DrayManager'
 
 describe('DrayManager', () => {
-    let manager = new DrayManager();
+	let manager = new DrayManager();
 
-    describe('createJob', () => {
-       it('creates DrayJob instance with parameters set', () => {
-           let job = manager.createJob({name: 'foo'});
-           expect(job.name).to.equal('foo');
-       });
-    });
+	describe('createJob', () => {
+		it('creates DrayJob instance with parameters set', () => {
+			let job = manager.createJob({name: 'foo'});
+			expect(job.name).to.equal('foo');
+		});
+	});
 
-    describe('listJobs', () => {
-        it('resolves DrayJob instances', () => {
-            manager._request = sinon.stub().returns(
-                bluebird.Promise.resolve({
-                    res: {
-                        body: [
-                            {id: 'foo'},
-                            {id: 'bar'}
-                        ]
-                    }
-                })
-            );
-            let jobs = manager.listJobs();
-            return bluebird.Promise.all([
-                expect(jobs).to.eventually.have.length(2)
-            ]);
-        });
-    });
+	describe('listJobs', () => {
+		it('resolves DrayJob instances', () => {
+			manager._request = sinon.stub().returns(
+				bluebird.Promise.resolve({
+					res: {
+						body: [
+							{id: 'foo'},
+							{id: 'bar'}
+						]
+					}
+				})
+			);
+			let jobs = manager.listJobs();
+			return bluebird.Promise.all([
+				expect(jobs).to.eventually.have.length(2)
+			]);
+		});
+	});
 });
